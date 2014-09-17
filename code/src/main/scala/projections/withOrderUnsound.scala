63bf4cc1c7a7ebf0e45b4ec2fbe6f334bb7cd39f
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-09-17 18:13:22 +0200

    projections with with cause withering

--- code/src/main/scala/projections/withOrderUnsound.scala
+++ code/src/main/scala/projections/withOrderUnsound.scala
@@ -4,8 +4,12 @@ object WithOrderUnsound {
 
     trait A { type E }
 
-    trait C extends A { type E = Int }
-    trait D extends A { type E = String }
+    trait C extends A { final type E = Int }
+    trait D extends A { final type E = String }
+
+    trait Caution
+
+    type Use = Any with Caution
 
     type EE[X <: A] = X#E
    
@@ -21,10 +25,14 @@ object WithOrderUnsound {
 
     def dc[A0 <: D with C]: A0#E = 1
 
+    def argh[C0 <: C, D0 <: D, E0 <: C0#E with D0#E]: E0 = ???
+
     trait UnsoundessIsComing {
 
       type X <: D with C
 
       val uh = cd[D with C]
+
+      val argadfad = argh[C,D,String with Int]
     }
 }
