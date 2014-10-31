1603ba84f79e0bd3cc17bf23923b85e49e241076
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-10-31 17:41:39 +0100

    add refs, note about adapting tuples

--- code/src/main/scala/errors.scala
+++ code/src/main/scala/errors.scala
@@ -4,6 +4,18 @@ package ohnosequences.scalaguide.errors
 ## Error handling
 
 Adapted and expanded from the [typelevel blog](http://typelevel.org/blog/2014/02/21/error-handling.html).
+
+#### refs
+
+Just for myself for now
+
+- http://eed3si9n.com/learning-scalaz/Either.html
+- http://www.slideshare.net/normation/nrm-scala-iocorrectlymanagingerrorsinscalav13
+- https://stackoverflow.com/questions/20065853/validation-versus-disjunction
+- https://stackoverflow.com/questions/21170322/scala-silently-catch-all-exceptions
+- http://www.sumologic.com/blog/company/why-you-should-never-catch-throwable-in-scala
+- https://stackoverflow.com/questions/12307965/method-parameters-validation-in-scala-with-for-comprehension-and-monads/12309023#12309023
+
 */
 
 import scalaz._
@@ -18,7 +30,7 @@ object Wonky extends Validate[(String, String), Wonky]{
 
   val validate: Input => Out = input => {
 
-    // ugly I know
+    // ugly I know. See also https://issues.scala-lang.org/browse/SI-5898
     val (five: String, palindrome: String) = input
 
     if (five.size != 5) -\/(MustHaveLengthFive(five))
