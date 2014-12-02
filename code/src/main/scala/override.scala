1f27f8de6578df60a3fdf81b52b24ddd10efda92
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-12-02 15:55:47 +0100

    override example

--- code/src/main/scala/override.scala
+++ code/src/main/scala/override.scala
@@ -1,12 +1,57 @@
 package ohnosequences.scalaguide.nooverride
 
-trait T[X,Y] {
-  def soAbstract(x: X, y: Y)
+class X
+class Y
+
+object withOverride {
+
+  object otherCompilationUnit {
+
+      trait ImprovT {
+
+      def soAbstract(x: X, y: Y): Int = 3
+    }
+    trait T extends ImprovT {
+
+      def soAbstract(x: X, y: Y): Int
+    }
+  }
+
+  /* In this scope we only know about `T` */
+  import otherCompilationUnit.T
+
+  abstract class C extends T {
+      
+    /*
+    And we happily modify what was deemed to be 3 at a more abstract level!
+    */
+    override def soAbstract(x: X, y: Y): Int = 2
+  }
 }
 
-abstract class C[X,Y] extends T[X,Y] {
-  //I think that I implement soAbsract
-  def soAbstract(y: Y, x: X) {
-    //but I do not
+object withoutOverride {
+
+  object otherCompilationUnit {
+
+      trait ImprovT {
+
+      def soAbstract(x: X, y: Y): Int = 3
+    }
+    trait T extends ImprovT {
+
+      def soAbstract(x: X, y: Y): Int
+    }
   }
+
+  /* In this scope we only know about `T` */
+  import otherCompilationUnit.T
+
+  abstract class C extends T {
+      
+    /*
+    In this case we get a compiler error: you need override if you want to override
+    */
+    // def soAbstract(x: X, y: Y): Int = 2
+  }
+
 }
