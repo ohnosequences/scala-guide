fa2d2ecc2f153acffae8a7efe30310602268dd8d
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-01-08 17:18:03 +0100

    first cut at no-cost derived structures; example with Fields

--- src/main/scala/typeclasses.scala
+++ src/main/scala/typeclasses.scala
@@ -23,7 +23,6 @@ object algs {
   /* 
   We use denotations to recognize at compile-time that a type (`T#Raw` in this case) is used to denote `T`. For example, the additive monoid for integers. We would have something like `IntPlusMonoid <: AnyAlg`, which could be denoted by `Int`s; we would have to specify then `IntPlusMonoid#Raw = Int`.
   */
-  type U[T <: AnyAlg] = T#Raw Denotes T
 
   /*
   A way of thinking about ops is as the real signature for your type, abstracted over the type used to denote it.
@@ -44,9 +43,9 @@ object algs {
     // refine the algebra type: a group
     type Alg <: AnyGroup
 
-    def unit: U[Alg]
-    def mu(arg1: U[Alg], arg2: U[Alg]): U[Alg]
-    def inverse(arg: U[Alg]): U[Alg]
+    def unit: Alg#Raw Denotes Alg
+    def mu(arg1: Alg#Raw Denotes Alg, arg2: Alg#Raw Denotes Alg): Alg#Raw Denotes Alg
+    def inverse(arg: Alg#Raw Denotes Alg): Alg#Raw Denotes Alg
   }
   trait GroupOps[G <: AnyGroup] extends AnyGroupOps { type Alg = G }
 
@@ -60,7 +59,7 @@ object algs {
   /* the companion object provides syntax */
   object StdGroupSyntax {
 
-    implicit def syntax[R <: G#Raw, G <: AnyGroup](g: R Denotes G)(implicit ops: AnyGroupOps { type Alg = G }): StdGroupSyntax[G] = new StdGroupSyntax[G](g.value)
+    implicit def syntax[R <: G#Raw, G <: AnyGroup, V <% R Denotes G](g: V)(implicit ops: AnyGroupOps { type Alg = G }): StdGroupSyntax[G] = new StdGroupSyntax[G](g.value)
   }
 
   /* define your syntax here, through a value class */
@@ -68,11 +67,11 @@ object algs {
 
     type Tpe = G
     // in theory we should get the type from the value
-    final def +(other: U[G])(implicit 
+    final def plus(other: G#Raw Denotes G)(implicit 
       group: G,
       groupOps: GroupOps[G]
     )
-    : U[G] = groupOps.mu(group := value, other)
+    : G#Raw Denotes G = groupOps.mu(group := value, other)
   }
 
   trait AnyGroupModule { module =>
@@ -80,38 +79,100 @@ object algs {
     type Alg <: AnyGroup
     type Ops <: AnyGroupOps { type Alg = module.Alg }
 
-    implicit val ops: Ops
   }
 
-  object IntPlusModule extends AnyGroupModule {
+  object IntPlusModule {
 
     type Alg = IntPlus.type
     type Ops = IntPlusOps.type
-
-    implicit val ops = IntPlusOps
+    implicit def addSyntax[V <% (IntPlus.type#Raw Denotes IntPlus.type)](v: V): StdGroupSyntax[IntPlus.type] = new StdGroupSyntax[IntPlus.type](v.value)
   }
+  object IntPlus extends Group[Int] { 
 
-  object IntPlus extends Group[Int] {
+    val label = "IntPlus" 
 
-    val label = "IntPlus"
+    implicit val intPlusOps: IntPlusOps.type = IntPlusOps
   }
-
   object IntPlusOps extends GroupOps[IntPlus.type] {
 
     final def unit: Alg := Int = IntPlus := 0
-    final def mu(arg1: U[Alg], arg2: U[Alg]): U[Alg] = IntPlus := arg1.value + arg2.value
-    final def inverse(arg: U[Alg]): U[Alg] = IntPlus := - arg.value
+    final def mu(arg1: Alg#Raw Denotes Alg, arg2: Alg#Raw Denotes Alg): Alg#Raw Denotes Alg = IntPlus := arg1.value + arg2.value
+    final def inverse(arg: Alg#Raw Denotes Alg): Alg#Raw Denotes Alg = IntPlus := - arg.value
   }
+
+  object IntMultModule {
+    type Alg = IntMult.type
+    type Ops = IntMultOps.type
+    implicit val ops: Ops = IntMultOps
+  }
+  object IntMult extends Group[Int] { val label = "IntMult" }
+  object IntMultOps extends GroupOps[IntMult.type] {
+
+    final def unit: Alg := Int = IntMult := 1
+    final def mu(arg1: Alg#Raw Denotes Alg, arg2: Alg#Raw Denotes Alg): Alg#Raw Denotes Alg = IntMult := arg1.value * arg2.value
+    final def inverse(arg: Alg#Raw Denotes Alg): Alg#Raw Denotes Alg = IntMult :=  1 / arg.value
+  }
+
+  /* define your syntax here, through a value class */
+  final class StdMultGroupSyntax[G <: AnyGroup](val value: G#Raw) extends AnyVal with AnyGroupSyntax {
+
+    type Tpe = G
+    // in theory we should get the type from the value
+    final def *(other:G#Raw Denotes G)(implicit 
+      group: G,
+      groupOps: GroupOps[G]
+    )
+    : G#Raw Denotes G = groupOps.mu(group := value, other)
+  }
+
+  object StdMultGroupSyntax {
+
+    implicit def fromIntMult[V <% (IntMult.type#Raw Denotes IntMult.type)](v: V): StdMultGroupSyntax[IntMult.type] = new StdMultGroupSyntax(v.value)
+  }
+
+
+
+
+
 
   // TODO: the Field example reusing ops etc
-  trait AnyField extends AnyAlg {
+  trait AnyField extends AnyAlg { field =>
 
-    type SumGroup <: AnyGroup
-    type MultGroup <: AnyGroup
+    type AddGroup <: AnyGroup { type Raw = field.Raw }
+    val addGroup: AddGroup
+    type MultGroup <: AnyGroup { type Raw = field.Raw }
+    val multGroup: MultGroup
+
+    // this is a bit weird, but works
+    implicit def toAddGroup[R <% Raw Denotes field.type](f: R): Raw Denotes AddGroup = addGroup := f.value
+    implicit def toMultGroup[R <% Raw Denotes field.type](f: R): Raw Denotes MultGroup = multGroup := f.value
+
+    implicit def toMeFromAdd(ag: Raw Denotes AddGroup): Raw Denotes field.type = new Denotes(ag.value)
+    implicit def toMeFromMult(mg: Raw Denotes MultGroup): Raw Denotes field.type = new Denotes(mg.value)
   }
 
-  trait AnyFieldOps extends AnyAlgOps
+  class Field[ 
+    R,
+    AG <: AnyGroup { type Raw = R },
+    MG <: AnyGroup { type Raw = R }
+  ]
+  (val addGroup: AG, val multGroup: MG, val label: String) extends AnyField {
 
+    type Raw = R
+    type AddGroup = AG
+    type MultGroup = MG
+  }
+
+  object AnyField {
+
+    // coercions
+    
+  }
+
+  object IntField extends Field[Int, IntPlus.type, IntMult.type](IntPlus, IntMult, "IntField") {
+
+    implicit val me: IntField.type = IntField
+  }
 }
 
 
--- src/test/scala/monoidsExample.scala
+++ src/test/scala/monoidsExample.scala
@@ -10,12 +10,37 @@ class groupOpsAndSyntax extends org.scalatest.FunSuite {
     import StdGroupSyntax._
 
     val two = IntPlus := 2
-    val four = two + two
+    val four = two plus two
 
     assert {
 
       four === (IntPlus := 4)
     }
+  }
+
+  test("syntax for fields") {
+
+    import ohnosequences.cosas.types._
+
+    import IntField._
+    import StdGroupSyntax._
+    import AnyField._
+    import StdMultGroupSyntax._
+
+    val two = IntField := 2
+
+    val oo = IntMult := 2
+
+    import IntMultModule._
+
+    val four = two * two
+
+    import IntPlusModule._
+
+    val fourAgain = two plus two
+
+    // val reallyFour: U[IntField.type] = four
+    val eight = fourAgain plus four
   }
 
 }
