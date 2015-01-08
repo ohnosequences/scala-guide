4e146075c07efd0d43a9a0e7ffd68743d7587b89
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-01-08 17:20:49 +0100

    reduce imports

--- src/test/scala/monoidsExample.scala
+++ src/test/scala/monoidsExample.scala
@@ -23,23 +23,15 @@ class groupOpsAndSyntax extends org.scalatest.FunSuite {
     import ohnosequences.cosas.types._
 
     import IntField._
-    import StdGroupSyntax._
-    import AnyField._
     import StdMultGroupSyntax._
-
-    val two = IntField := 2
-
-    val oo = IntMult := 2
-
     import IntMultModule._
-
-    val four = two * two
-
     import IntPlusModule._
 
-    val fourAgain = two plus two
 
-    // val reallyFour: U[IntField.type] = four
+    val two = IntField := 2
+    val oo = IntMult := 2
+    val four = two * two
+    val fourAgain = two plus two
     val eight = fourAgain plus four
   }
 
